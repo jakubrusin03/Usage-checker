c6d50f7f3bba775da6208ff53c92717886775b05
JakubRusin03 <jakub.d.rusin@gmail.com>
2026-05-13 18:24:50 +0000

    fix: fix cpu command

--- d_check.sh
+++ d_check.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 dysk=$(df -h / | grep / | awk '{print $5}' | tr -d "%")
-cpu=$(top -bn1 | grep "Cpu(s)" | awk '{print  100 - $8 }')
+cpu=$(top -bn1 | grep "Cpu(s)" | awk '{print int(100 - $8)}')
 ram=$(free -h | grep "Mem:" | awk '{print $7}' | tr -d "Gi")
 
 for i in {1..10}; do
   data=$(date)
-  echo "STAN NA: $data"
-  echo "-------------"
+  echo "STAN NA: $data" >> /home/labuser/skrypt_dysk/test.log
+  echo "-------------" >> /home/labuser/skrypt_dysk/test.log
   if (( dysk >= 90 )); then
     echo "KRYTYCZNY: DYSK ZAPELNIONY W: $dysk%" >> /home/labuser/skrypt_dysk/test.log #/var/log/d_check.log
   elif (( dysk >= 80 )); then
@@ -15,7 +15,8 @@ for i in {1..10}; do
   else
    echo "OK: DYSK ZAPELNIONY W: $dysk%" >> /home/labuser/skrypt_dysk/test.log #/var/log/d_check.log
   fi
-  echo "-------------"
+  echo "-------------" >> /home/labuser/skrypt_dysk/test.log
+
   if (( cpu >= 90 )); then
    echo "KRYTYCZNY: CPU UZYWANE W: $cpu%" >> /home/labuser/skrypt_dysk/test.log
   elif (( cpu >= 80 )); then
